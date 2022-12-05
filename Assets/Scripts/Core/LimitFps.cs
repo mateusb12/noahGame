6ccffd215f06649c27f6dd0df562d36ba56516f4
mateusb12 <mateus_b09@hotmail.com>
2022-12-05 12:49:39 -0300

    Limit fps

---
+++ Assets/Scripts/Core/LimitFps.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class LimitFps: MonoBehaviour
+    {
+        private void Start()
+        {
+            // Set the target frame rate to 60 FPS
+            Application.targetFrameRate = 60;
+        }
+    }
+}
